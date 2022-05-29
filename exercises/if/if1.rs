28693072807915d5d00238b9055a6d424ad72189
Borer <2927602652@qq.com>
2022-05-29 17:24:01 +0800

    fix if

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -4,6 +4,11 @@
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
+    if a > b {
+        a
+    } else {
+        b
+    }
     // Do not use:
     // - another function call
     // - additional variables
