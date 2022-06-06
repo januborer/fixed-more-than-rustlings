47a0598036443958949df74233ff82bf5e0b0d04
Borer <2927602652@qq.com>
2022-06-06 20:11:44 +0800

    modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -8,21 +8,34 @@
 mod delicious_snacks {
 
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
-
-    mod fruits {
-        pub const PEAR: &'static str = "Pear";
-        pub const APPLE: &'static str = "Apple";
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+    mod test {
+        const TEST: &'static str = "Test";
+        pub fn test() {
+            println!("{}", TEST);
+        }
     }
 
-    mod veggies {
+    pub mod fruits {
+        pub const PEAR: &'static str = "Pear";
+        pub const APPLE: &'static str = "Apple";
+        pub fn print_test() {
+            super::test::test();
+        }
+    }
+
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
 }
 
+use delicious_snacks::fruit;
+use delicious_snacks::veggie;
+
 fn main() {
+    delicious_snacks::fruits::print_test();
     println!(
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
